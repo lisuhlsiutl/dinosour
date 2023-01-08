3b0409867cd02809a7e764d18fea94dbd740eb20
lionaprokhorenko <liona.prokhorenko@icloud.com>
2023-01-08 14:38:00 +0300

    rar

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import pygame
 import sys
+import os
+bg_image = pygame.image.load(os.path.join('files', 'bg.png'))
+bg_pos = bg_x, bg_y = 0, 500
+bg_speed = 5
+
+def bg(screen):
+    global bg_x, bg_y
+    image_w = bg_image.get_width()
+    bg_x -= bg_speed
+    if bg_x <= -image_w:
+        bg_x = image_w
+    screen.blit(bg_image, (bg_x, bg_y))
+    if bg_x <= 1000 - image_w:
+        screen.blit(bg_image, (bg_x + image_w - 1, bg_y))
+
+def update(screen):
+    bg(screen)
+
 
 def main():
     pygame.init()
@@ -10,11 +28,13 @@ def main():
     clock = pygame.time.Clock()
     while pygame.event.wait().type != pygame.QUIT:
         screen.fill((255, 255, 255))
-
+        update(screen)
         pygame.display.flip()
-        clock.tick(50)
+        clock.tick(30)
 
     pygame.quit()
+
+
 
 
 
